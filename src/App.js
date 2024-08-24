3bb58ea5e7f686c3028bfbc637be00c655cb880d
Richard Ojok <richieee@live.co.uk>
2024-08-24 19:55:16 +0100

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is hosted in Azure within the Microsoft App Services
+          This is hosted in Azure within the Microsoft App Services!
         </p>
         <a
           className="App-link"
